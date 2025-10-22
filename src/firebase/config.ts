6b35343001b3b79f8b18684f3774ff465219a3b2
Ifeoluwa Sotola <sotolaifeoluwa@gmail.com>
2025-10-22 00:35:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5424960404-8c6bd",
+  "appId": "1:1038651140243:web:b93f3dfa32c91e581b0566",
+  "apiKey": "AIzaSyAz-PzWRlUcPbCliDdWd0ILD7RU-6bGVaM",
+  "authDomain": "studio-5424960404-8c6bd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1038651140243"
+};
